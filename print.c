33626eab8e4186c4addaf63aee8143ac81ecffe0
Hildibjørg Didriksen <s164539@student.dtu.dk>
2021-04-23 08:28:21 +0200

    Added the print function that adds the board

---
+++ print.c
@@ -0,0 +1,26 @@
+//
+// Created by Hildibjorg on 23.04.2021.
+//
+
+
+#include <stdio.h>
+
+int main() {
+    int input;
+    printf("C1\tC2\tC3\tC4\tC5\tC6\tC7\n"
+           "\n"
+           "[]\t[]\t[]\t[]\t[]\t[]\t[]\t\t[]\tF1\n"
+           "[]\t[]\t[]\t[]\t[]\t[]\t[]\n"
+           "[]\t[]\t[]\t[]\t[]\t[]\t[]\t\t[]\tF2\n"
+           "[]\t[]\t[]\t[]\t[]\t[]\t[]\n"
+           "[]\t[]\t[]\t[]\t[]\t[]\t[]\t\t[]\tF3\n"
+           "[]\t[]\t[]\t[]\t[]\t[]\t[]\n"
+           "[]\t[]\t[]\t[]\t[]\t[]\t[]\t\t[]\tF4\n"
+           "[]\t[]\t[]\n"
+           "\n"
+           "LAST Command:\n"
+           "Message: \n"
+           "INPUT >"
+    );
+    scanf("%d",&input);
+}
