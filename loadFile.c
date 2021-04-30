6c2389ffe41eb4dc7fd4530fc5646294dff07f01
Hildibjørg Didriksen <s164539@student.dtu.dk>
2021-04-30 10:41:43 +0200

    Added loadFile

---
+++ loadFile.c
@@ -0,0 +1,32 @@
+//
+// Created by Hildibjorg on 23.04.2021.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+
+int main() {
+    const char *filename = "Deck.txt";
+    char input[42];
+    printf("Enter a file name:\n");
+    /*
+     * C:\\Users\\Hildibjorg\\Desktop\\Deck.txt
+     */
+
+    fgets(input,sizeof(input),stdin);
+
+
+
+    FILE *infile = fopen(input, "r");
+    struct stat sb;
+    stat(filename, &sb);
+
+    char *file_contents = malloc(sb.st_size);
+    while (fscanf(infile, "%[^\n] ", file_contents) != EOF){
+        printf("> %s\n", file_contents);
+    }
+
+    fclose(infile);
+    exit(EXIT_SUCCESS);
+}
