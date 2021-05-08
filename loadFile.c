30ebc37df4e2c814ce053e82fa85d201c571b283
Hildibjørg Didriksen <s164539@student.dtu.dk>
2021-05-08 20:00:15 +0200

    loadFile can now save a filepath of unknown length and saves the content of the given file

--- loadFile.c
+++ loadFile.c
@@ -7,26 +7,35 @@
 #include <sys/stat.h>
 
 int main() {
-    const char *filename = "Deck.txt";
-    char input[41];
+    FILE *infile;
+    int c;
+    char input[100] = "";
+
     printf("Enter a file name:\n");
-    /*
-     * C:\\Users\\Hildibjorg\\Desktop\\Deck.txt
-     */
+    // Test path: C:\\Users\\Hildibjorg\\Desktop\\Deck.txt
 
-    fgets(input,sizeof(input),stdin);
-    printf("%s \n",input);
+    //scans and saves the given filepath
+    scanf("%s",&input);
 
+    //opens the file and saves the content in input
+    infile = fopen(input, "r");
 
-    FILE *infile = fopen(input, "r");
-    struct stat sb;
-    stat(filename, &sb);
-
-    char *file_contents = malloc(sb.st_size);
-    while (fscanf(infile, "%[^\n] ", file_contents) != EOF){
-        printf("> %s\n", file_contents);
+    //Print error message if loading the file fails
+    if(infile == NULL){
+        printf("Error in opening file");
+        return(-1);
     }
 
+    // prints the content of the given file:
+    while(1) {
+        c = fgetc(infile);
+        if(feof(infile)){
+            break;
+        }
+        printf("%c",c);
+    }
+
+
     fclose(infile);
-    exit(EXIT_SUCCESS);
+    return(0);
 }
