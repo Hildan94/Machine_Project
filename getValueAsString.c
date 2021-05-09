247b91866d70bfd58aecec8b74a10e6f62d9cca1
Hildibjørg Didriksen <s164539@student.dtu.dk>
2021-05-10 01:23:43 +0200

    Updated and works properly

--- getValueAsString.c
+++ getValueAsString.c
@@ -16,26 +16,26 @@ int main() {
 char getValueAsString(int number) {
     char result='b';
 
-    switch (number) {
-        case 0:
-            result = 'A';
-            break;
-        case 9:
-            result = 'T';
-            break;
-        case 10:
-            result = 'J';
-            break;
-        case 11:
-            result = 'D';
-            break;
-        case 12:
-            result = 'K';
-            break;
-        default:
-            result = (number+1)+'0';
-            break;
-    }
+        switch (number) {
+            case 1:
+                result = 'A';
+                break;
+            case 10:
+                result = 'T';
+                break;
+            case 11:
+                result = 'J';
+                break;
+            case 12:
+                result = 'D';
+                break;
+            case 13:
+                result = 'K';
+                break;
+            default:
+                result = (number) + '0';
+                break;
+        }
 
-    return result;
-}
+        return result;
+    }
